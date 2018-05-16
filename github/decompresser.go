666a80aa04e2a0805956fe28fd6844355a82f3b5
ktr0731 <aizu.s1230022@gmail.com>
2018-05-16 22:41:06 +0900

    split decompresser logic

--- github/decompresser.go
+++ github/decompresser.go
@@ -12,16 +12,20 @@ type Decompresser func(io.Reader) (io.Reader, error)
 
 var (
 	DefaultDecompresser              = TarGZIPDecompresser
-	TarDecompresser     Decompresser = func(r io.Reader) (io.Reader, error) {
-		tr := tar.NewReader(r)
-		_, err := tr.Next()
-		return tr, errors.Wrap(err, "failed to create tar reader")
-	}
-	TarGZIPDecompresser Decompresser = func(r io.Reader) (io.Reader, error) {
-		gr, err := gzip.NewReader(r)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to create GZIP reader")
-		}
-		return TarDecompresser(gr)
-	}
+	TarDecompresser     Decompresser = tarDecompresser
+	TarGZIPDecompresser Decompresser = tarGZIPDecompresser
 )
+
+func tarDecompresser(r io.Reader) (io.Reader, error) {
+	tr := tar.NewReader(r)
+	_, err := tr.Next()
+	return tr, errors.Wrap(err, "failed to create tar reader")
+}
+
+func tarGZIPDecompresser(r io.Reader) (io.Reader, error) {
+	gr, err := gzip.NewReader(r)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create GZIP reader")
+	}
+	return TarDecompresser(gr)
+}
