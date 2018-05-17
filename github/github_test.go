9d1de02b42565b2c9f21ba01aafb9a1c9d1666f0
ktr0731 <aizu.s1230022@gmail.com>
2018-05-17 17:17:04 +0900

    fix tests

--- github/github_test.go
+++ github/github_test.go
@@ -20,7 +20,7 @@ var (
 
 func TestGitHubReleaseMeans_LatestTag(t *testing.T) {
 	// TODO: don't use real http request
-	c, err := GitHubReleaseMeans("ktr0731", "evans")()
+	c, err := GitHubReleaseMeans("ktr0731", "evans", TarDecompresser)()
 	require.NoError(t, err)
 	_, err = c.LatestTag(context.Background())
 	assert.NoError(t, err)
